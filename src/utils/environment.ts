589b45d9396ab7702027faa2bbc83ef0a73ce982
Leonardo Venturini <leovenbag@gmail.com>
2022-07-30 21:07:19 -0400

    fix process error

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,9 +1,9 @@
+if (typeof process === 'undefined') {
+  // @ts-ignore
+  window.process = {}
+}
+
 export namespace Environment {
   export const isNode = !!process?.versions?.node
   export const isBrowser = typeof window === 'object'
-  export const isTest = process.env.NODE_ENV === 'test'
-  export const isDevelopment = process.env.NODE_ENV === 'development'
-  export const isProduction = process.env.NODE_ENV === 'production'
-  export const isDebug = process.env.DEBUG === '1'
-  export const protocol = isProduction ? 'https' : 'http'
 }
