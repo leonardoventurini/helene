875d3af20a82494549ca65f392dd5c7123bd14a5
Leonardo Venturini <leovenbag@gmail.com>
2023-03-23 16:40:40 -0400

    add data hooks

---
+++ src/react/hooks/use-data.tsx
@@ -0,0 +1,126 @@
+import { useAsyncEffect, useCreation, useDebounceFn } from 'ahooks'
+import { useClient, useCombinedDebounce, useRawEventObservable } from './index'
+import { useCallback, useState } from 'react'
+import { set } from 'lodash'
+import { v5 as uuidv5 } from 'uuid'
+import { BrowserStorage } from '../../data/browser'
+import { useFind } from './use-find'
+import { ClientEvents } from '../../utils'
+import { createCollection } from '../../data'
+
+const browserStorage = new BrowserStorage()
+
+type Props = {
+  method: string
+  params?: any
+  filter?: Record<string, any>
+  sort?: Record<string, 1 | -1>
+  projection?: Record<string, 0 | 1>
+  selectiveSync?: boolean
+  authenticated?: boolean
+}
+
+export function useData({
+  method,
+  params,
+  filter,
+  sort,
+  projection,
+  selectiveSync = false,
+  authenticated = false,
+}: Props) {
+  const [collection, setCollection] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const client = useClient()
+
+  const result: any = useCreation(() => ({}), [])
+
+  const data = useFind({
+    collection,
+    filter,
+    sort,
+    projection,
+  })
+
+  const refresh = useDebounceFn(
+    async () => {
+      if (!collection) return
+      if (authenticated && !client.authenticated) {
+        setLoading(false)
+        return
+      }
+
+      const count = await collection.count({})
+
+      if (!data.length) {
+        setLoading(true)
+      }
+
+      let response
+
+      if (count && selectiveSync) {
+        const [{ updatedAt: lastUpdatedAt = null } = {}] = (await collection
+          .find({})
+          .projection({ updatedAt: 1 })
+          .sort({ updatedAt: -1 })) ?? [{}]
+
+        response = await client.call(method, { ...params, lastUpdatedAt })
+
+        for (const datum of Array.isArray(response) ? response : [response]) {
+          if (await collection.findOne({ _id: datum._id })) {
+            await collection.remove({ _id: datum._id })
+          }
+        }
+      } else {
+        response = await client.call(method, params)
+
+        await collection.remove({}, { multi: true })
+      }
+
+      if (response) {
+        await collection.insert(response)
+      }
+
+      setLoading(false)
+    },
+    { wait: 100, leading: false },
+  )
+
+  useAsyncEffect(async () => {
+    const collection = await createCollection({
+      name: `collection:${uuidv5(method, uuidv5.URL)}`,
+      storage: browserStorage,
+      timestamps: true,
+      autoload: true,
+    })
+
+    set(window, `collections.${method}`, collection)
+
+    setCollection(collection)
+
+    await refresh.run()
+  }, [method])
+
+  const initialized$ = useRawEventObservable(client, ClientEvents.INITIALIZED)
+  const contextChanged$ = useRawEventObservable(
+    client,
+    ClientEvents.CONTEXT_CHANGED,
+  )
+
+  useCombinedDebounce({
+    observables: [initialized$, contextChanged$],
+    debounce: 500,
+    callback: useCallback(() => {
+      refresh.run()
+    }, [refresh.run]),
+  })
+
+  result.collection = collection
+  result.data = data
+  result.loading = loading
+  result.client = client
+  result.refresh = refresh.run
+
+  return result
+}
---
+++ src/react/hooks/use-find.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import { Collection, CollectionEvent } from '../../data'
+
+type Options = {
+  collection: Collection
+  filter?: Record<string, any>
+  sort?: Record<string, 1 | -1>
+  projection?: Record<string, 0 | 1>
+}
+
+export function useFind({ collection, filter, sort, projection }: Options) {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    if (!collection) return
+
+    async function onUpdated() {
+      setData(await collection.find(filter, projection).sort(sort))
+    }
+
+    onUpdated().catch(console.error)
+
+    collection.on(CollectionEvent.UPDATED, onUpdated)
+
+    return () => {
+      collection.off(CollectionEvent.UPDATED, onUpdated)
+    }
+  }, [
+    collection,
+    JSON.stringify(filter),
+    JSON.stringify(projection),
+    JSON.stringify(sort),
+  ])
+
+  return data
+}
