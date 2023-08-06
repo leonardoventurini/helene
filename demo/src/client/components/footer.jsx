fed8a6aa8889f27710c4ed80ed9e9a4b5b9741e5
Leonardo Venturini <leovenbag@gmail.com>
2023-08-06 10:26:45 -0400

    adjust footer

--- demo/src/client/components/footer.jsx
+++ demo/src/client/components/footer.jsx
@@ -1,9 +1,9 @@
 export function Footer() {
   return (
-    <footer className='footer bg-neutral text-neutral-content mt-16 items-center justify-center p-8'>
+    <footer className='footer text-neutral-content mt-16 items-center justify-center p-8'>
       <div className='prose flex flex-col items-center'>
+        <p className='m-0'>Copyright © 2022 Leonardo Venturini</p>
         <p className='m-0'>
-          Released under the{' '}
           <a
             href='https://github.com/leonardoventurini/helene/blob/main/LICENSE'
             target='_blank'
@@ -11,10 +11,6 @@ export function Footer() {
           >
             MIT License
           </a>
-        </p>
-        <p className='m-0'>
-          Copyright © {new Date().getFullYear()} Leonardo Venturini &
-          Contributors
         </p>
       </div>
     </footer>
