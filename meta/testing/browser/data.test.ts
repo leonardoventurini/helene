2b342178587836a9b4399da8095f32e40a0294d7
Leonardo Venturini <leovenbag@gmail.com>
2024-03-30 20:22:11 -0400

    add idb storage

--- meta/testing/browser/data.test.ts
+++ meta/testing/browser/data.test.ts
@@ -1,6 +1,7 @@
 import { Collection, createCollection } from '@helenejs/data'
-import { BrowserStorage } from '@helenejs/data/lib/browser'
+import { BrowserStorage, IDBStorage } from '@helenejs/data/lib/browser'
 import { expect } from 'chai'
+import { get } from 'idb-keyval'
 
 type Test = { _id: number; name: string }
 
@@ -30,6 +31,40 @@ describe('Helene Data', function () {
       const collection2 = await createCollection<Test>({
         name: 'test',
         storage: new BrowserStorage(),
+        autoload: true,
+      })
+
+      const docs = await collection2.find()
+
+      expect(docs).to.have.length(10)
+    })
+  })
+
+  describe('IDB Storage', () => {
+    let collection: Collection<Test>
+
+    before(async () => {
+      Error.stackTraceLimit = Infinity
+
+      collection = await createCollection<Test>({
+        name: 'test',
+        storage: new IDBStorage(),
+      })
+    })
+
+    it('inserting & finding', async () => {
+      for (let i = 0; i <= 9; i++) {
+        await collection.insert({ _id: i, name: `test_${i}` })
+      }
+
+      const data = await get('helene:data:test')
+
+      expect(data).to.include('test_0')
+      expect(data).to.include('test_9')
+
+      const collection2 = await createCollection<Test>({
+        name: 'test',
+        storage: new IDBStorage(),
         autoload: true,
       })
 
