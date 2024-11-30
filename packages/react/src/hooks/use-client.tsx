ae0c2120163dab3d2cbeea3dddc87b8030bfa3b6
Leonardo Venturini <leovenbag@gmail.com>
2024-11-30 17:19:17 -0400

    add types to client hook

--- packages/react/src/hooks/use-client.tsx
+++ packages/react/src/hooks/use-client.tsx
@@ -1,9 +1,11 @@
+import { Client } from '@helenejs/client'
+import { Environment, ServerMethods } from '@helenejs/utils'
 import { useContext } from 'react'
 import { ClientContext } from '../components'
-import { Client } from '@helenejs/client'
-import { Environment } from '@helenejs/utils'
 
-export const useClient = (): Client => {
+export function useClient<
+  T extends ServerMethods = ServerMethods,
+>(): Client<T> {
   const client = useContext(ClientContext)
 
   if (Environment.isServer) return null
